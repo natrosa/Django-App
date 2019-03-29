126c86cfe4090bf04d68cb8bde03be1a272f7a75
Natasha <natrosdev12@gmail.com>
2019-03-28 20:32:50 -0400

    updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['natdjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['natdjangoapp.herokuapp.com', 'https://natdjangoapp.herokuapp.com/']
 
 # Application definition
 
